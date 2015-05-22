70fdb70b8fa053f42731a7410a4c574efe9a10b0
Love Bisaria <bisarialove@gmail.com>
2015-05-22 01:39:53 -0700

    My first OpenCL code

---
+++ hello_world.h
@@ -0,0 +1,24 @@
+//
+//  hello_world.h
+//  HelloWorld
+//
+//  Created by Love Bisaria on 1/24/15.
+//
+//
+
+#ifndef __HelloWorld__hello_world__
+#define __HelloWorld__hello_world__
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <OpenCL/opencl.h>
+
+#define DATA_SIZE (1024)
+
+#endif /* defined(__HelloWorld__hello_world__) */
